809d04db4f460ae26270bbdc7a2ee0a76d7bf68f
陈创慧 <3086178063@qq.com>
2024-05-29 14:26:28 +0800

    a suggestion of SC model

--- src/model/SC.java
+++ src/model/SC.java
@@ -13,6 +13,27 @@ public class SC implements Serializable {
 	private String Cno;//课程号
 	private String Cname;//课程名称
 	private double Grade;//成绩
+/*
+	//这里或者省略关于学生的信息和课程信息而后新建学生类对象和课程类对象
+	private Course c;//课程
+	private Student s;//学生
+	public Course getCourse(){return c;}
+	public void setCourse(Course course){
+		c.setCcredit(course.getCcredit());
+		c.setCname(course.getCname());
+		c.setCno(course.getCno());
+		c.setCteacher(course.getCteacher());
+	}
+	public Student getStudent(){return s;}
+	public void setStudent(Student student){
+		s.setClno(student.getClno());
+		s.setSage(student.getSage());
+		s.setSname(student.getSname());
+		s.setSsex(student.getSsex());
+		s.setSno(student.getSno());
+	}
+
+ */
 	
 	public String getCname() {
 		return Cname;
